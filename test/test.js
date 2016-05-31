2e17c62cd92fcc8873c85801f89a104244205281
Reid <jreid.greer@gmail.com>
2016-05-31 15:21:42 -0700

    Unignore test directory

---
+++ test/test.js
@@ -0,0 +1,5 @@
+describe('Ultra DOM Manipulation', function() {
+  // it('respond with matching records', function() {
+  //   return db.find({ type: 'User' }).should.eventually.have.length(3);
+  // });
+});
