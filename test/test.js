93c1ec650b64b7873d013c1056417844cccb3362
Reid <jreid.greer@gmail.com>
2016-05-31 15:38:00 -0700

    Implement basic tests

--- test/test.js
+++ test/test.js
@@ -1,5 +1,16 @@
 describe('Ultra DOM Manipulation', function() {
-  // it('respond with matching records', function() {
-  //   return db.find({ type: 'User' }).should.eventually.have.length(3);
-  // });
+  it('Can find and store reference to a class', function() {
+    var test = ultra('.test');
+    expect(test._domRef).to.be.an('object');
+  });
+
+  it('Can find and store reference to a ID', function() {
+    var test = ultra('#test');
+    expect(test._domRef).to.be.an('object');
+  });
+
+  it('Can store a collection of elements by class', function() {
+    var test = ultra('.test_multiple');
+    expect(test._domRef).to.be.an('array');
+  });
 });
