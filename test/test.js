47d0510f8bc3a8fae6c1a5a5696d6b8b8c5927cc
Reid <jreid.greer@gmail.com>
2016-06-05 18:41:02 -0700

    Better implement testing and instantiation

--- test/test.js
+++ test/test.js
@@ -1,16 +1,17 @@
 describe('Ultra DOM Manipulation', function() {
   it('Can find and store reference to a class', function() {
     var test = ultra('.test');
-    expect(test._domRef).to.be.an('object');
+    console.log('test', typeof test.ref());
+    expect(test.ref()).to.be.an('object');
   });
 
   it('Can find and store reference to a ID', function() {
     var test = ultra('#test');
-    expect(test._domRef).to.be.an('object');
+    expect(test.ref()).to.be.an('object');
   });
 
   it('Can store a collection of elements by class', function() {
     var test = ultra('.test_multiple');
-    expect(test._domRef).to.be.an('array');
+    expect(test.ref()).to.be.an('array');
   });
 });
--- ultra.js
+++ ultra.js
@@ -1,25 +1,31 @@
 // Basic DOM manipulation library
 
 var ultra = function(input) {
+  var _domRef;
+  var eventStorage;
+
+  if(input.charAt(0) === '.') {
+    _domRef = document.getElementsByClassName(input.slice(1));
+    // If refering to only a single element, remove from array
+    _domRef = _domRef.length === 1 ? _domRef[0] : _domRef;
+  } else if(input.charAt(0) === '#') {
+    _domRef = document.getElementById(input.slice(1));
+  } else {
+    _domRef = document.createElement(input);
+  }
+
   var ultraInstance = function() {
-    this._domRef;
-    this.eventStorage;
-
-    if(input.charAt(0) === '.') {
-      _domRef = document.getElementsByClassName(input.slice(1));
-      // If refering to only a single element, remove from array
-      _domRef = _domRef.length === 1 ? _domRef[0] : _domRef;
-    } else if(input.charAt(0) === '#') {
-      _domRef = document.getElementById(input.slice(1));
-    } else {
-      _domRef = document.createElement(input);
-    }
   };
 
-  ultraInstance.prototype.click = function(callback) {
-    this.eventStorage.click = !this.eventStorage.click ? this.eventStorage.click = [] : this.eventStorage.click;
-    this.eventStorage.click.push(callback);
+  ultraInstance.click = function(callback) {
+    eventStorage.click = !eventStorage.click ? eventStorage.click = [] : eventStorage.click;
+    eventStorage.click.push(callback);
   };
+
+  ultraInstance.ref = function() {
+    return _domRef;
+  };
+
 
   return ultraInstance;
 }
